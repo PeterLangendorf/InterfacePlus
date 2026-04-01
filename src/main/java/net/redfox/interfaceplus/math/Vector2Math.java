b5155a47a1ca252577f65c6c3782052d07aa7821
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-04-01 13:06:41 -0400

    Simplify some math in Vector2Math

--- src/main/java/net/redfox/interfaceplus/math/Vector2Math.java
+++ src/main/java/net/redfox/interfaceplus/math/Vector2Math.java
@@ -10,9 +10,7 @@ public class Vector2Math {
      * @return A double containing the distance from the starting point to the ending point.
      */
     private static double getDistanceBetweenTwoPoints(Vector2 origin, Vector2 goal) {
-        return Math.sqrt(
-                Math.pow(goal.getX() - origin.getX(), 2)
-                        + Math.pow(goal.getY() - origin.getY(), 2));
+        return Math.hypot(goal.getX() - origin.getX(), goal.getY() - origin.getY());
     }
 
     /**
@@ -23,31 +21,27 @@ public class Vector2Math {
      * @return A double containing the direction in radians.
      */
     public static double getDirectionToPoint(Vector2 origin, Vector2 goal) {
-        return Math.atan(goal.getY() - origin.getY() / goal.getX() - origin.getX());
+        return Math.atan2(goal.getY() - origin.getY(), goal.getX() - origin.getX());
     }
 
     /**
-     * Calculates x and y increments that need to be added to your current x and y position to move
-     * speed units in radians direction
+     * Calculates x and y increments that need to be added to your current x and y position to move speed units in radians direction
      *
      * @param radians The angle in radians that you are traveling
      * @param speed The speed at which you are going
-     * @return A Vector2 containing the increments that should be added to your position to move
-     *     speed units in radians direction
+     * @return A Vector2 containing the increments that should be added to your position to move speed units in radians direction
      */
     public static Vector2 getIncrementsBySpeed(double radians, double speed) {
         return new Vector2(speed * Math.cos(radians), speed * Math.sin(radians));
     }
 
     /**
-     * Calculates the increments required to be added to a position in order to move speed units
-     * towards goal from origin
+     * Calculates the increments required to be added to a position in order to move speed units towards goal from origin
      *
      * @param origin The starting position
      * @param goal The ending position
      * @param speed The speed at which the object is moving
-     * @return A Vector2 containing the increments that should be added to your position to move
-     *     speed units in radians direction
+     * @return A Vector2 containing the increments that should be added to your position to move speed units in radians direction
      */
     public static Vector2 getIncrementsFromPoint(Vector2 origin, Vector2 goal, double speed) {
         double distance = getDistanceBetweenTwoPoints(origin, goal);
