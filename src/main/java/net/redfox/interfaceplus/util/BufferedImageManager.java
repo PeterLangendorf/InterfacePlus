afb7a8ff2b0e35c7c95a6631cf3a7f2e595b12b5
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-31 22:46:16 -0400

    Add javadoc to getImage

--- src/main/java/net/redfox/interfaceplus/util/BufferedImageManager.java
+++ src/main/java/net/redfox/interfaceplus/util/BufferedImageManager.java
@@ -1,5 +1,10 @@
 package net.redfox.interfaceplus.util;
 
+import net.redfox.interfaceplus.gui.util.WindowContext;
+import net.redfox.interfaceplus.math.Size2;
+import net.redfox.interfaceplus.math.Vector2;
+
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,24 +12,30 @@ import javax.imageio.ImageIO;
 
 @SuppressWarnings("unused")
 public class BufferedImageManager {
-    private static final Logger LOGGER = new Logger(BufferedImageManager.class.getName());
+	private BufferedImageManager() {}
 
-    public static BufferedImage getImage(String imgPath) {
-        try {
-            InputStream stream = BufferedImageManager.class.getResourceAsStream(imgPath);
-            if (stream == null) {
-                LOGGER.warn(
-                        "An image at '"
-                                + imgPath
-                                + "' was not found! Some renderable objects may appear invisible!");
-            } else {
-                LOGGER.info("Image at '" + imgPath + "' was successfully loaded!");
-                return ImageIO.read(stream);
-            }
-        } catch (IOException _) {
-        }
-        return new BufferedImage(1, 1, 1);
-    }
+	private static final Logger LOGGER = new Logger(BufferedImageManager.class.getName());
+
+	/**
+	 * Creates a new {@link BufferedImage} containing the image provided by the {@link String} input path. If the path is invalid, returns an empty {@link BufferedImage}
+	 * @param imgPath The path to the image, in the resource folder, with / as the resources directory.
+	 * @return A new {@link BufferedImage} using the path provided.
+	 */
+	public static BufferedImage getImage(String imgPath) {
+		try {
+			InputStream stream = BufferedImageManager.class.getResourceAsStream(imgPath);
+			if (stream == null) {
+				LOGGER.warn(
+						"An image at '" + imgPath + "' was not found! Some renderable objects may appear invisible!");
+			} else {
+				LOGGER.info("Image at '" + imgPath + "' was successfully loaded!");
+				return ImageIO.read(stream);
+			}
+		} catch (IOException _) {
+		}
+		return new BufferedImage(1, 1, 1);
+	}
+
 	/**
 	 * Draws the specified {@link BufferedImage} to the screen. This method must be called every loop, as images are cleared between each frame update.
 	 * @param graphics The {@link Graphics2D} object used to draw the image. Can be obtained via {@link WindowContext#getGraphics2D()}
