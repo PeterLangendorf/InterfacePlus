74bcae51330df487084628f1698e57b1afb1d51e
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-31 22:46:04 -0400

    Add javadocs to drawImage

--- src/main/java/net/redfox/interfaceplus/util/BufferedImageManager.java
+++ src/main/java/net/redfox/interfaceplus/util/BufferedImageManager.java
@@ -25,6 +25,13 @@ public class BufferedImageManager {
         }
         return new BufferedImage(1, 1, 1);
     }
+	/**
+	 * Draws the specified {@link BufferedImage} to the screen. This method must be called every loop, as images are cleared between each frame update.
+	 * @param graphics The {@link Graphics2D} object used to draw the image. Can be obtained via {@link WindowContext#getGraphics2D()}
+	 * @param image The {@link BufferedImage} that is displayed.
+	 * @param pos The position at which the image is displayed.
+	 * @param size The legal bounds of the image being displayed.
+	 */
 	public static void drawImage(Graphics2D graphics, BufferedImage image, Vector2 pos, Size2 size) {
 		graphics.drawImage(image, (int) (pos.getX() + 0.5), (int) (pos.getY() + 0.5),
 				size.getWidth(), size.getHeight(), null);
