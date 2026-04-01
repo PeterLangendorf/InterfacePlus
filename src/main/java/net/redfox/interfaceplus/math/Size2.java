90e4ef69815f83490d7296d4439ec6d423681b07
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-31 22:44:20 -0400

    Rewrite Size2 and Vector2 classes

--- src/main/java/net/redfox/interfaceplus/math/Size2.java
+++ src/main/java/net/redfox/interfaceplus/math/Size2.java
@@ -1,3 +1,33 @@
 package net.redfox.interfaceplus.math;
 
-public record Size2(int width, int height) {}
+import java.awt.Dimension;
+
+public class Size2 {
+	private int width;
+	private int height;
+
+	public Size2() {
+		this(0, 0);
+	}
+
+	public Size2(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
