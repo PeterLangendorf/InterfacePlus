1b9222f79cfff462d3e4d0b0f8ab1d1813fadbbe
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-31 22:42:44 -0400

    Format & convert Dimension -> Size2

--- src/main/java/net/redfox/interfaceplus/gui/DisplayPanel.java
+++ src/main/java/net/redfox/interfaceplus/gui/DisplayPanel.java
@@ -6,66 +6,68 @@ import net.redfox.interfaceplus.gui.display.Interface;
 import net.redfox.interfaceplus.gui.util.KeyHandler;
 import net.redfox.interfaceplus.gui.util.MouseHandler;
 import net.redfox.interfaceplus.gui.util.WindowContext;
+import net.redfox.interfaceplus.math.Size2;
 import net.redfox.interfaceplus.object.Renderer;
 
 public class DisplayPanel extends JPanel implements Runnable {
-    public final Dimension size;
-    private final int fps;
+	public final Size2 size;
+	private final int fps;
 
-    private Thread thread;
-    private final Interface panelInterface;
-    private final WindowContext context;
-    public final Renderer renderer;
+	private Thread thread;
+	private final Interface panelInterface;
+	private final WindowContext context;
+	public final Renderer renderer;
 
-    public DisplayPanel(Interface i, int fps, Dimension size) {
-        this.size = size;
-        this.fps = fps;
-        context = new WindowContext();
-        this.setDoubleBuffered(true);
-        this.addKeyListener(new KeyHandler());
-        this.addMouseListener(new MouseHandler());
-        this.setFocusable(true);
-        this.panelInterface = i;
-        this.renderer = new Renderer();
-    }
+	public DisplayPanel(Interface i, int fps, Size2 size) {
+		this.size = size;
+		this.fps = fps;
+		context = new WindowContext();
+		this.setDoubleBuffered(true);
+		this.addKeyListener(new KeyHandler());
+		this.addMouseListener(new MouseHandler());
+		this.setFocusable(true);
+		this.panelInterface = i;
+		this.renderer = new Renderer();
+	}
 
-    public void startGameThread() {
-        thread = new Thread(this);
-        thread.start();
-    }
+	public void startGameThread() {
+		thread = new Thread(this);
+		thread.start();
+	}
 
-    @Override
-    public void run() {
-        double drawInterval = 1000000000.0 / fps;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
+	@Override
+	public void run() {
+		double drawInterval = 1000000000.0 / fps;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
 
-        while (thread != null) {
-            currentTime = System.nanoTime();
-            delta += (currentTime - lastTime) / drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
+		while (thread != null) {
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / drawInterval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
 
-            if (delta >= 1) {
-                panelInterface.update();
-                repaint();
+			if (delta >= 1) {
+				panelInterface.update();
+				repaint();
 
-                delta--;
-            }
-            if (timer >= 1000000000) {
-                timer = 0;
-            }
-        }
-    }
+				delta--;
+			}
+			if (timer >= 1000000000) {
+				timer = 0;
+			}
+		}
+	}
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        context.setG2(g2);
-        context.setLocation(this.getLocationOnScreen());
-        if (context.isUsable()) panelInterface.updateImages(context);
-        g2.dispose();
-    }
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		context.setG2(g2);
+		context.setLocation(this.getLocationOnScreen());
+		if (context.isUsable()) panelInterface.updateImages(context);
+		g2.dispose();
+	}
 }
--- src/main/java/net/redfox/interfaceplus/gui/display/Interface.java
+++ src/main/java/net/redfox/interfaceplus/gui/display/Interface.java
@@ -5,94 +5,95 @@ import java.util.ArrayList;
 import javax.swing.*;
 import net.redfox.interfaceplus.gui.DisplayPanel;
 import net.redfox.interfaceplus.gui.util.WindowContext;
+import net.redfox.interfaceplus.math.Size2;
 import net.redfox.interfaceplus.object.Renderer;
 import net.redfox.interfaceplus.task.Task;
 
 public class Interface {
-    private static int totalInterfaces = 0;
-    private final int identifier;
-    private final ArrayList<Task> taskList = new ArrayList<>();
-    private final JFrame window;
-    private final DisplayPanel panel;
+	private static int totalInterfaces = 0;
+	private final int identifier;
+	private final ArrayList<Task> taskList = new ArrayList<>();
+	private final JFrame window;
+	private final DisplayPanel panel;
 
-    protected Interface(String title, Dimension size, Color background, int FPS) {
-        window = new JFrame();
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	protected Interface(String title, Size2 size, Color background, int FPS) {
+		window = new JFrame();
+		window.setResizable(false);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel = new DisplayPanel(this, FPS, size);
-        window.add(panel);
-        window.setTitle(title);
-        panel.setBackground(background);
-        panel.setPreferredSize(size);
-        window.pack();
-        window.setLocationRelativeTo(null);
+		panel = new DisplayPanel(this, FPS, size);
+		window.add(panel);
+		window.setTitle(title);
+		panel.setBackground(background);
+		panel.setPreferredSize(size.toDimension());
+		window.pack();
+		window.setLocationRelativeTo(null);
 
-        window.setVisible(true);
+		window.setVisible(true);
 
-        panel.startGameThread();
+		panel.startGameThread();
 
-        identifier = totalInterfaces;
-        totalInterfaces++;
-    }
+		identifier = totalInterfaces;
+		totalInterfaces++;
+	}
 
-    public void update() {
-        for (Task t : taskList) {
-            t.execute();
-        }
-    }
+	public void update() {
+		for (Task t : taskList) {
+			t.execute();
+		}
+	}
 
-    public void updateImages(WindowContext context) {
-        panel.renderer.renderObjects(context);
-    }
+	public void updateImages(WindowContext context) {
+		panel.renderer.renderObjects(context);
+	}
 
-    public void addTask(Task t) {
-        taskList.add(t);
-    }
+	public void addTask(Task t) {
+		taskList.add(t);
+	}
 
-    public Renderer getRenderer() {
-        return panel.renderer;
-    }
+	public Renderer getRenderer() {
+		return panel.renderer;
+	}
 
-    public int getIdentifier() {
-        return identifier;
-    }
+	public int getIdentifier() {
+		return identifier;
+	}
 
-    public static class Builder {
-        private String title;
-        private Dimension size;
-        private Color background;
-        private int FPS;
+	public static class Builder {
+		private String title;
+		private Size2 size;
+		private Color background;
+		private int FPS;
 
-        public Builder() {
-            title = "Untitled";
-            size = new Dimension(1920, 1080);
-            background = Color.WHITE;
-            FPS = 60;
-        }
+		public Builder() {
+			title = "Untitled";
+			size = new Size2(1920, 1080);
+			background = Color.WHITE;
+			FPS = 60;
+		}
 
-        public Builder title(String title) {
-            this.title = title;
-            return this;
-        }
+		public Builder title(String title) {
+			this.title = title;
+			return this;
+		}
 
-        public Builder color(Color background) {
-            this.background = background;
-            return this;
-        }
+		public Builder color(Color background) {
+			this.background = background;
+			return this;
+		}
 
-        public Builder size(Dimension size) {
-            this.size = size;
-            return this;
-        }
+		public Builder size(Size2 size) {
+			this.size = size;
+			return this;
+		}
 
-        public Builder FPS(int FPS) {
-            this.FPS = FPS;
-            return this;
-        }
+		public Builder FPS(int FPS) {
+			this.FPS = FPS;
+			return this;
+		}
 
-        public Interface build() {
-            return new Interface(title, size, background, FPS);
-        }
-    }
+		public Interface build() {
+			return new Interface(title, size, background, FPS);
+		}
+	}
 }
